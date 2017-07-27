26deab4693d49c700d262bb1e02d46245d8d9484
Malte Langosz <malte.langosz@dfki.de>
2017-07-27 16:56:45 +0200

    Fix for specific autoproj configuration

--- buildconf.py
+++ buildconf.py
@@ -88,7 +88,12 @@ def checkBaseName(package, info):
 def clonePackage(cfg, package, server, gitPackage, branch):
     clonePath = package
     if package[-2:] == ".*":
-        clonePath = "/".join(package.split("/")[:-1])+"/" + gitPackage.split("/")[1].split(".")[0]
+        print package + " " + gitPackage
+        arrPackage = package.split("/")[:-1]
+        p = gitPackage.split("/")[1].split(".")[0]
+        if arrPackage[-1] != p:
+            arrPackage.append(p)
+        clonePath = "/".join(arrPackage)
     if package in cfg["updated"]:
         return False
     else:
